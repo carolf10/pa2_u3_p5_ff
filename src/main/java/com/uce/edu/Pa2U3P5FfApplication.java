f7d5d0b12f95b59843a46d3be4ab5e4e4d1cf0e3
carolf10 <fcarofialloscheca@hotmail.com>
2024-02-19 21:05:58 -0500

    Taller N°35

--- src/main/java/com/uce/edu/Pa2U3P5FfApplication.java
+++ src/main/java/com/uce/edu/Pa2U3P5FfApplication.java
@@ -1,16 +1,20 @@
 package com.uce.edu;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.uce.edu.ventas.service.IFacturaService;
+import com.uce.edu.ventas.repository.modelo.Cliente;
+import com.uce.edu.ventas.service.IClienteService;
 
 @SpringBootApplication
 public class Pa2U3P5FfApplication implements CommandLineRunner{
 	@Autowired
-	private IFacturaService facturaService;
+	private IClienteService clienteService;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U3P5FfApplication.class, args);
@@ -19,11 +23,55 @@ public class Pa2U3P5FfApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-//		System.out.println("SUPPORTS -------------------");
-//		this.facturaService.pruebaSupports();
+		
+//		System.out.println("Nombre Hilo: " + Thread.currentThread().getName());
+//		//Significa que vamos revisar los hilos de ejecucion  
 //		
-		System.out.println("NEVER -------------------");
-		//this.facturaService.pruebaNever();
+//		long timepoInicial = System.currentTimeMillis();
+//		for(int i=1; i<=10;i++) {
+//			Cliente cliente= new Cliente();
+//			cliente.setNombre("CN"+i);
+//			cliente.setApellido("CA"+i);
+//			this.clienteService.guardar(cliente);
+//		}
+//		long timepoFinal = System.currentTimeMillis();
+//		long tiempoTranscurrido = (timepoFinal - timepoInicial)/1000;
+//		System.out.println("Tiempo transcorrido en seg: " + tiempoTranscurrido);
+//		//101	
+		
+//		System.out.println("Nombre Hilo: " + Thread.currentThread().getName());
+//		long timepoInicial = System.currentTimeMillis();
+//		List<Cliente> listaCliente = new ArrayList<>();
+//		for(int i=1; i<= 100;i++) {
+//			Cliente cliente= new Cliente();
+//			cliente.setNombre("CN"+i);
+//			cliente.setApellido("CA"+i);
+//			listaCliente.add(cliente);
+//		}
+//		
+//		listaCliente.stream().forEach(cliente -> this.clienteService.guardar(cliente));
+//		long timepoFinal = System.currentTimeMillis();
+//		long tiempoTranscurrido = (timepoFinal - timepoInicial)/1000;
+//		System.out.println("Tiempo transcorrido en seg: " + tiempoTranscurrido);
+//		//101	
+		
+		System.out.println("Nombre Hilo: " + Thread.currentThread().getName());
+		long timepoInicial = System.currentTimeMillis();
+		List<Cliente> listaCliente = new ArrayList<>();
+		for(int i=1; i<= 100;i++) {
+			Cliente cliente= new Cliente();
+			cliente.setNombre("CN"+i);
+			cliente.setApellido("CA"+i);
+			listaCliente.add(cliente);
+		}
+		
+		listaCliente.parallelStream().forEach(cliente -> this.clienteService.guardar(cliente));
+		long timepoFinal = System.currentTimeMillis();
+		long tiempoTranscurrido = (timepoFinal - timepoInicial)/1000;
+		System.out.println("Tiempo transcorrido en seg: " + tiempoTranscurrido);
+		//14
+		
+		
 		
 	}
 
--- src/main/java/com/uce/edu/ventas/service/ClienteServiceImpl.java
+++ src/main/java/com/uce/edu/ventas/service/ClienteServiceImpl.java
@@ -1,5 +1,7 @@
 package com.uce.edu.ventas.service;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
@@ -21,6 +23,17 @@ public class ClienteServiceImpl implements IClienteService {
 		// TODO Auto-generated method stub
 		try{
 			this.clienteRepository.insertar(cliente);
+			try {
+				// Buscar en el registro civil
+				// Validar no tenga deudas
+				// Validar
+				// ...
+				System.out.println("Nombre Hilo: " + Thread.currentThread().getName());
+				TimeUnit.SECONDS.sleep(1);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}catch (RuntimeException e) {
 			// TODO: handle exception
 			System.out.println(e.getClass());
